0dfebe96648e25a4d5bb8b5aee3dd66c26e73b25
renan-almeida <renanalmeidadantas2007@gmail.com>
2025-04-06 14:37:13 -0300

    Desfazendo conflitos

--- ConectaProApp/MauiProgram.cs
+++ ConectaProApp/MauiProgram.cs
@@ -1,31 +1,27 @@
 ﻿using Microsoft.Extensions.Logging;
-<<<<<<< HEAD
-using CommunityToolkit.Maui;
-=======
 using Mopups.Hosting;
 
->>>>>>> 02/04_RenanAlmeida
 namespace ConectaProApp;
 
 public static class MauiProgram
 {
-	public static MauiApp CreateMauiApp()
-	{
-		var builder = MauiApp.CreateBuilder();
-		builder
-			.UseMauiApp<App>()
-			.UseMauiCommunityToolkit()
-			.ConfigureFonts(fonts =>
-			{
-				fonts.AddFont("OpenSans-Regular.ttf", "OpenSansRegular");
-				fonts.AddFont("OpenSans-Semibold.ttf", "OpenSansSemibold");
-			})
-			.ConfigureMopups();
+    public static MauiApp CreateMauiApp()
+    {
+        var builder = MauiApp.CreateBuilder();
+
+        builder
+            .UseMauiApp<App>()
+            .ConfigureFonts(fonts =>
+            {
+                fonts.AddFont("OpenSans-Regular.ttf", "OpenSansRegular");
+                fonts.AddFont("OpenSans-Semibold.ttf", "OpenSansSemibold");
+            })
+            .ConfigureMopups(); // Mopups corretamente encadeado
 
 #if DEBUG
-		builder.Logging.AddDebug();
+        builder.Logging.AddDebug();
 #endif
 
-		return builder.Build();
-	}
+        return builder.Build();
+    }
 }
