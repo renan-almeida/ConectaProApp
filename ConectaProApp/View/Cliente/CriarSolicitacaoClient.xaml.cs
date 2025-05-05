851d35106dc193304b5cf24c1ba0da6726e8ca03
renan-almeida <renanalmeidadantas2007@gmail.com>
2025-05-05 16:36:40 -0300

    ### Criação da View da Segunda Etapa da criação de um serviço
    ### Criação da View da Etapa final da criação de um serviço
    
    ### Bindings adicionados na ViewModel de Criação de serviço
    
    ### Permissão para o cliente realizar o upload de uma imagem do serviço em andamento.

--- ConectaProApp/View/Cliente/CriarSolicitacaoClient.xaml.cs
+++ ConectaProApp/View/Cliente/CriarSolicitacaoClient.xaml.cs
@@ -1,10 +1,15 @@
+using ConectaProApp.ViewModels.Cliente;
+
 namespace ConectaProApp.View.Cliente;
 
 public partial class CriarSolicitacaoClient : ContentPage
 {
+    private CriarSolicitacaoViewModel _criarSolicitacaoViewModel;
 	public CriarSolicitacaoClient()
 	{
 		InitializeComponent();
+        _criarSolicitacaoViewModel = new CriarSolicitacaoViewModel();
+        BindingContext = _criarSolicitacaoViewModel;
 	}
 
     private void OnFotoEmpresaClicked(object sender, EventArgs e)
--- ConectaProApp/ViewModels/Cliente/CriarSolicitacaoViewModel.cs
+++ ConectaProApp/ViewModels/Cliente/CriarSolicitacaoViewModel.cs
@@ -1,12 +1,70 @@
-﻿using System;
+﻿using ConectaProApp.Services.Servico;
+using ConectaProApp.View.Cliente.CriarSolicitacaoViews;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Windows.Input;
 
 namespace ConectaProApp.ViewModels.Cliente
 {
-    class CriarSolicitacaoViewModel
+    public class CriarSolicitacaoViewModel: BaseViewModel
     {
+        private ServicoService sService;
+        public ICommand EtapaDoisCommand { get; set; }
+        public ICommand EtapaFinalCommand { get; set; }
+
+        public CriarSolicitacaoViewModel()
+        {
+            sService = new ServicoService();
+            InitializeCommands();
+        }
+
+        private void InitializeCommands()
+        {
+            EtapaDoisCommand = new Command(async() => await EtapaDois());
+            EtapaFinalCommand = new Command(async() =>  await EtapaFinal());
+        }
+
+        public async Task EtapaDois()
+        {
+            try
+            {
+                var proximaPagina = new CriarSolicitacaoClientTwo
+                {
+                    BindingContext = this
+                };
+
+
+                await Application.Current.MainPage.Navigation.PushAsync(proximaPagina);
+            }
+            catch (Exception ex)
+            {
+                await Application.Current.MainPage.DisplayAlert
+                       ("Erro!", ex.Message + "Detalhes: " + ex.InnerException, "Ok");
+
+            }
+        }
+
+        public async Task EtapaFinal()
+        {
+            try
+            {
+                var proximaPagina = new CriarSolicitacaoClientFinal
+                {
+                    BindingContext = this
+                };
+
+
+                await Application.Current.MainPage.Navigation.PushAsync(proximaPagina);
+            }
+            catch (Exception ex)
+            {
+                await Application.Current.MainPage.DisplayAlert
+                       ("Erro!", ex.Message + "Detalhes: " + ex.InnerException, "Ok");
+
+            }
+        }
     }
 }
